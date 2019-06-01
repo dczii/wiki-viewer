3ea11e8048c07f72e18b7edfcf956905279f2db0
Zii <dczii@live.com>
2019-06-01 14:24:41 +0800

    Apply React Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
@@ -6,38 +6,31 @@ import TextField from 'material-ui/TextField';
 import SearchWebIcon from 'mdi-react/SearchWebIcon';
 import ResultList from './components/ResultList';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchField: false,
-      searchValue: '',
-      results: []
-    };
-  }  
+function App(props) {
+  let [searchField, handleSearchField] = useState(false);
+  let [searchValue, handleSearchValue] = useState('');
+  let [results, handleResults] = useState([]);
 
-  onSearch = () => {
-    let val = this.state.searchValue
+  function onSearch() {
+    let val = searchValue
     if(val !== '') {
-      this.handleSearch(val)
+      handleSearch(val)
     } else {
-      this.setState({searchField: !this.state.searchField})
+      handleSearchField(!searchField)
     }
   }
 
-  onSearchText = (e) => {
-    this.setState({ searchValue: e.target.value })
-    this.handleSearch(e.target.value)
+  function onSearchText(e) {
+    handleSearchValue(e.target.value)
+    handleSearch(e.target.value)
   }
 
-  handleSearch(searchTerm) {
+  function handleSearch(searchTerm) {
     fetch(`https://cors.io/?http://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${searchTerm}&format=json`)
     .then(res => res.json())
     .then(
       (result) => {
-        this.setState({
-          results: result.query.search
-        })
+        handleResults(result.query.search)
       },
       (error) => {
         console.log(error)
@@ -45,15 +38,15 @@ class App extends Component {
     )
   }
 
-  render() {
-    var search = this.state.searchField ? <TextField className='App-search'
-                hintText="Type to search Wikipedia.."
-                inputStyle={{color: '#FFF'}}
-                hintStyle={{color: '#FFF'}}
-                onChange={this.onSearchText.bind(this)}
-                onSubmit={this.handleSearch.bind(this, this.state.searchValue)}
-                value={this.state.searchValue}
-              /> : null;
+  var search = searchField ? <TextField className='App-search'
+      hintText="Type to search Wikipedia.."
+      inputStyle={{color: '#FFF'}}
+      hintStyle={{color: '#FFF'}}
+      onChange={onSearchText.bind(this)}
+      onSubmit={handleSearch.bind(this, searchValue)}
+      value={searchValue}
+    /> : null;
+
     return (
       <MuiThemeProvider>
         <div className="App">
@@ -62,15 +55,15 @@ class App extends Component {
           </div>
           <div className="App-body">
             <div>
-              <FloatingActionButton onClick={this.onSearch}>
+              <FloatingActionButton onClick={() => onSearch()}>
                 <SearchWebIcon /> 
               </FloatingActionButton>
             </div>
             <div>
                 {search}
             </div>
-            {this.state.results ? 
-              <ResultList results={this.state.results}/>
+            {results ? 
+              <ResultList results={results}/>
             : '' }
           </div>
           <div className="footer">
@@ -79,7 +72,7 @@ class App extends Component {
         </div>
       </MuiThemeProvider>
     );
-  }
+
 }
 
 export default App;
